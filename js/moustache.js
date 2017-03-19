7615bc25610d83bce2fe5c570be1ad05125bf7aa
mikemjharris <mike.mj.harris@gmail.com>
2017-03-19 18:14:52 +0000

    whitespace

--- js/moustache.js
+++ js/moustache.js
@@ -1,43 +1,33 @@
 $(document).ready(function() {
 
+  for (var i = 1; i < 42; i++) {
+    var imgadd = "img_" + i + ".jpg";
+    var linkadd = '<img class="allpic" src=../img/' + imgadd + ' />';
+    $(".picbox").append(linkadd);
+  }
 
-		for (var i = 1; i < 42; i++) {
-		var imgadd = "img_" + i + ".jpg";
-		var linkadd = '<img class="allpic" src=../img/' + imgadd + ' />';
+  $('.allpic').hide();
+  $('.allpic:nth-child(1)').show();
 
-		$(".picbox").append(linkadd);
+  var p = 1;
 
-		}
+  function displayimage(p,q) {
+    if (p === 1) {
+        q = 1;
+    }
+    if (p === 41) {
+        q = -1;
+    }
+    
+    var $thischild = $(".allpic:nth-child(" + p + ")");
+    var $nextchild = $(".allpic:nth-child(" + (p + q) + ")");
+    var timeInt = 300;
 
-                $('.allpic').hide();
-                 $('.allpic:nth-child(1)').show();
+    $($thischild).fadeOut(500);
+    $($nextchild).fadeIn(500, function() {
+      displayimage(p + q, q);
+    } );
+  }
 
-
-                var p = 1;
-
-                function displayimage(p,q) {
-
-                if (p === 1) {
-                    q = 1
-                }
-                if (p === 41) {
-                    q = -1
-                }
-
-                var $thischild = $(".allpic:nth-child(" + p + ")")
-                 var $nextchild = $(".allpic:nth-child(" + (p + q) + ")")
-                var timeInt = 300;
-
-                        $($thischild).fadeOut(500);
-                        $($nextchild).fadeIn(500, function() {
-                          displayimage(p + q, q);
-                        } );
-                 }
-
-                 displayimage(1,1);
-
-
-
-
-
-	});
+  displayimage(1,1);
+});
